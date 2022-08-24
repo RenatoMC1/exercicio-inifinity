b1cde65746a1c53b08ba929f362dc66a0a0f3afd
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-08-24 10:10:52 -0300

    exercio - conversão em centímetro

---
+++ Exercicio1.py
@@ -0,0 +1,9 @@
+#Questão 01 - Faça um programa que converta metros para centímetros.
+
+unidade_metro = float(input("Digite a quantidade em metros: "))
+
+print(f"A quantidade em centímetros é: {unidade_metro*100} cm")
+
+
+
+
