dd15278b6d75a8b5b1c600d7265fa5de9cbac564
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-08-24 10:23:53 -0300

    Exercicio 2 - calcular a area do circulo

---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+#Questão 02 - Faça um programa que peça o raio de um círculo, calcule e mostre a sua área.
+
+raio = int(input("Digite o valor do raio do círculo: "))
+
+print(f"A área do círculo é:  {pow(raio,2)*3.14}")
