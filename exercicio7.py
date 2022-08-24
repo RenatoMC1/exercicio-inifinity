57b6b88a5e735d7a2c586bcbb5807a90c80e0bed
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-08-24 11:52:15 -0300

    Update exercicio7.py

--- exercicio7.py
+++ exercicio7.py
@@ -1,15 +1,15 @@
 peso = float(input("Digite seu peso: "))
 altura = float(input("Digite seu altura: "))
-IMC = peso / pow(altura,2)
+IMC = peso / pow(altura, 2)
 
 if IMC <= 18.5:
-    print("Você está abaixo do peso")
+    print(f"Você está abaixo do peso, pois seu IMC é {round(IMC,2)}")
 elif 18.5 < IMC < 24.9:
-    print("Você está com peso normal")
+    print(f"Você está com peso normal,pois seu IMC é {round(IMC,2)}")
 elif 25 < IMC < 29.9:
-    print("Você está sobrepeso")
+    print(f"Você está sobrepeso, pois seu IMC é {round(IMC,2)}")
 elif 30 < IMC < 39.9:
-    print("Você está Obeso Tipo I")
+    print(f"Você está Obeso Tipo I, pois seu IMC é {round(IMC,2)}")
 
 else:
-    print("Alerta: Obesidade móbida")
+    print(f"Alerta: Obesidade móbida, pois seu IMC é {round(IMC,2)}")
