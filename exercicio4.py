70e8e4497fac5b1a35cb5cac949183d869d74f38
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-08-24 10:45:11 -0300

    Exercicio 4 - conversão em Celsius para Fahrenheit

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+#Questão 04 - Faça um programa que peça a temperatura em graus Fahrenheit,transforme e mostre a temperatura em graus Celsius.
+#T (° C) = (68 ° F - 32) × 5/9 = 20 ° C
+
+Temperatura_Fahrenheit = float(input("Digite a temperatura em ºF : "))
+
+print(f"A temperatura em Celsius é: {(Temperatura_Fahrenheit-32) * 5/9} ºC")
+
