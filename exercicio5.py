159309b845d8fab25c73ddbc3a35472bf8eb9832
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-08-24 10:55:09 -0300

    EXERCICIO 5 - CONVERSÃO EM CELSIUS PARA FAHRENHEIT

---
+++ exercicio5.py
@@ -0,0 +1,5 @@
+# Questão 05 - Faça um programa que peça a temperatura em graus Celsius,transforme e mostre a temperatura em graus Fahrenheit.
+
+Temperatura_Celsius = float(input("Digite a temperatura em ºC : "))
+
+print(f"A temperatura em Fahrenheit é: {Temperatura_Celsius * 1.8 +32} ºF")
