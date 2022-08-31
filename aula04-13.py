508fc470982f9daaaebd181729617855baac2456
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-08-31 12:31:21 -0300

    Add files via upload

---
+++ aula04-13.py
@@ -0,0 +1,11 @@
+soma = 0
+contador = 1
+
+while contador <= 5:
+    num1 = int(input("Digite o primeiro valor: "))
+    soma = soma + num1
+    contador = contador + 1
+
+print(f"a soma é {soma}")
+
+
